cfda1a00cdcdd3ab54230c712eacb325f18b18bd
BEAVIS <bbeavis@bbeavis.com>
2026-09-10 10:28:16 +1000

    Initial version

--- AutomationEngine/Program.cs
+++ AutomationEngine/Program.cs
@@ -2,9 +2,22 @@ using AutomationEngine.Composition;
 using Serilog;
 using System.Reflection;
 
+var exeDirectory = AppContext.BaseDirectory;
+
+try
+{
+    Directory.SetCurrentDirectory(exeDirectory);
+    Directory.CreateDirectory(Path.Combine(exeDirectory, "Logs"));
+}
+catch
+{
+    // Fallback to default working directory if setting the service directory fails.
+}
+
 // Configure Serilog with structured logging
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(new ConfigurationBuilder()
+        .SetBasePath(exeDirectory)
         .AddJsonFile("appsettings.json", optional: true)
         .AddJsonFile("appsettings.Development.json", optional: true)
         .AddEnvironmentVariables()
@@ -13,17 +26,7 @@ Log.Logger = new LoggerConfiguration()
     .Enrich.WithProperty("Application", "AutomationEngine")
     .CreateLogger();
 
-try
-{
-    var exePath = Assembly.GetExecutingAssembly().Location;
-    var exeDirectory = Path.GetDirectoryName(exePath);
-    Directory.SetCurrentDirectory(exeDirectory);
-    Log.Debug("Application working directory set to {WorkingDirectory}", exeDirectory);
-}
-catch (Exception ex)
-{
-    Log.Warning(ex, "Failed to set working directory, continuing with default");
-}
+Log.Debug("Application working directory set to {WorkingDirectory}", Directory.GetCurrentDirectory());
 
 var builder = WebApplication.CreateBuilder(args);
 Log.Information("WebApplication builder created");
@@ -34,7 +37,7 @@ builder.Host
 
 Log.Information("Host configuration: WindowsService enabled, Serilog logging configured");
 Log.Information("Application version: {Version} | Framework: .NET 10 | Environment: {Environment}",
-    Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown", 
+    Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown",
     builder.Environment.EnvironmentName);
 
 builder.ConfigureAutomationEngineServices();
